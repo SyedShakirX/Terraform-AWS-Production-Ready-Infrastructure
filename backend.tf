8baa5ef6dfdd3b82d859caa4703c96a3aefe42c8
SyedShakirX <159051009+SyedShakirX@users.noreply.github.com>
2026-03-08 19:47:28 +0530

    Update S3 backend configuration in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "aws-project-using-terraform-state-file-bucket"
-    key          = "StateFile/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
+    bucket       = "Name_Of_your_AWS_S3_Bucket"
+    key          = "Directory/terraform.tfstate"
+    region       = "AWS_Region"
+    use_lockfile = true #State Lock
   }
 }
